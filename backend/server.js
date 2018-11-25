e19560b9bb110d29f8ec0d2f89a0641bbd43ef99
Alexander Ruliov <alexander.ruliov@gmail.com>
2018-11-25 14:52:28 +0300

    show clustered markers

---
+++ backend/server.js
@@ -0,0 +1,114 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	next();
+	/*
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+			"Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+	*/
+});
+
+let locations = null;
+
+(async function() {
+	// Connection URL
+	const url = process.env.MONGODB_URI;
+	// Database Name
+	const dbName = 'hashtagwth';
+	const client = new MongoClient(url, {
+		useNewUrlParser: true
+	});
+
+	try {
+		// Use connect method to connect to the Server
+		await client.connect();
+
+		const db = client.db(dbName);
+
+		await onConnected({db});
+
+		console.log('started at port 3000');
+
+		app.get('/locations.json', (req, res) => {
+			res.setHeader('Content-Type', 'application/json');
+			res.end(JSON.stringify(locations));
+		});
+	} catch (err) {
+		console.log(err.stack);
+	}
+
+	client.close();
+})();
+
+app.listen(3000);
+
+async function onConnected({db}) {
+	locations = [];
+
+	await processInstagram({db, locations});
+	//await processTwitter({db, locations});
+
+	//fs.writeFileSync(path.join(__dirname, 'locations.json'), JSON.stringify(locations));
+}
+
+async function processInstagram({db, locations}) {
+	let instagramCollection = db.collection('instPosts');
+	
+	let cursor = instagramCollection.find({
+		//date: { $gt: '2018-11-23T20:50:29Z' }
+	}).sort({ date: -1 });
+
+	await cursor.forEach(function(doc) {
+		if (!doc) return;
+		if (!doc.url) return;
+
+		let lat = parseFloat(doc.lat),
+		    lon = parseFloat(doc.lon);
+
+		if (!isFinite(lat) || !isFinite(lon)) return;
+
+		let obj = {
+			lat: lat,
+			lon: lon,
+			img: doc.url
+		};
+
+		if (doc.shortcode) {
+			obj.url = 'https://www.instagram.com/p/' + doc.shortcode;
+		}
+
+		locations.push(obj);
+	});
+}
+
+async function processTwitter({db, locations}) {
+	let collection = db.collection('posts');
+	
+	let cursor = collection.find({
+		//date: { $gt: '2018-11-23T20:50:29Z' }
+	}).sort({ date: -1 });
+
+	await cursor.forEach(function(doc) {
+		if (!doc) return;
+
+		let lat = parseFloat(doc.lat),
+		    lon = parseFloat(doc.lon);
+
+		if (!isFinite(lat) || !isFinite(lon)) return;
+
+		locations.push({
+			lat: lat,
+			lon: lon
+		});
+	});
+}
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -3,6 +3,12 @@ import 'leaflet/dist/images/marker-icon-2x.png';
 import 'leaflet/dist/images/marker-shadow.png';
 import 'leaflet/dist/leaflet.js';
 
+import 'leaflet.markercluster/dist/MarkerCluster.css';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
+import 'leaflet.markercluster/dist/leaflet.markercluster.js';
+
+import 'leaflet.heat/dist/leaflet-heat.js';
+
 import './css/app.less';
 
 import { consumeTemplates, instantiateTemplate } from './dom/templates.js';
@@ -23,6 +29,56 @@ onDomReady(function() {
 
 		cardsContainerEl.appendChild(cardEl);
 	}
+
+	(async function() {
+		let response = await fetch('http://localhost:3000/locations.json');
+		let locations = await response.json();
+
+		var markers = L.markerClusterGroup();
+		
+		locations.forEach(function({lat, lon, img, url}) {
+			let markerOptions = {};
+
+			if (img) {
+				markerOptions.icon = L.icon({
+					iconUrl: img,
+					iconSize: [32, 32],
+					iconAnchor: [16, 16],
+					popupAnchor: [0, -18],
+				});
+			}
+
+			let marker = L.marker(new L.LatLng(lat, lon), markerOptions);
+
+			if (img) {
+				let html = '<div class="x-post-popup">';
+
+				if (url) html += '<a href="' + url + '">';
+
+				html += '<img src="' + img + '">';
+
+				if (url) html += '</a>';
+
+				html += '</div>';
+
+				marker.bindPopup(html);
+			}
+
+			markers.addLayer(marker);
+		});
+
+		map.addLayer(markers);
+
+		let heatPoints = [];
+
+		locations.forEach(function({lat, lon}) {
+			heatPoints.push([lat, lon, 0.5]);
+		}); 
+
+		let heat = L.heatLayer(heatPoints, {radius: 20});
+
+		heat.addTo(map);
+	})();
 });
 
 function onDomReady(fun, options) {
